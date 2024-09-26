88bfdd5f3cd58ac5c28e0d193fadd880a89da156
justamanwho <dmytro.kostuyk@gmail.com>
2024-09-26 21:11:50 +0200

    added example of saving all countries at once

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# The_World_ultimate_bot
+Input: country name
+
+Output: 
+- Show on the map (photo)
+- Flag (photo)
+- Capital
+- Area
+- Population
+- Currency
+- Current Time
+
+
+Countries localizations were drawn by Wikimedia Commons
+So there is Wikimedia Commons and the user TUBS who is magnificently made the 
+drawing of the countires on the map. I want to get all of them at once for my 
+python project. The thing is, how do I make it so they are relatively the same 
+(on the map closed up to the specific country, the photo is squared)
+
--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-print('hello world')
+import requests
+from bs4 import BeautifulSoup
+
+countries = ['Uganda']
+
+for country in countries:
+    # Fetch the Wikimedia page HTML
+    url = f'https://commons.wikimedia.org/wiki/File:Location{country}.svg'
+    response = requests.get(url)
+
+    # Parse the HTML to find the actual file URL
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    # Find the link to the SVG file (usually in <a> tag with "original-file-link" class)
+    file_link = soup.find('a', {'class': 'internal'})  # "internal" class usually contains the link to the actual file
+
+    if file_link:
+        svg_url = file_link['href']  # Get the href and prepend 'https:'
+        print(f"Downloading {country} map from: {svg_url}")
+
+        # Download the actual SVG file
+        svg_data = requests.get(svg_url)
+
+        # Save the SVG file locally
+        with open(f'{country}.svg', 'wb') as file:
+            file.write(svg_data.content)
+            print(f"{country} map saved as {country}.svg")
+    else:
+        print(f"Could not find SVG file for {country}.")
+
+# Get all countries
+
+
+
